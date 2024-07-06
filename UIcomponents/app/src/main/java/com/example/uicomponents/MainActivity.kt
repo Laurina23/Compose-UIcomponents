2d9aa642e37d9114dbdc430d94f43b4ba820d96d
Laurina-p <laurinapatnaik20@gmail.com>
2024-07-06 22:55:16 +0530

    implemented alert dialog

--- UIcomponents/app/src/main/java/com/example/uicomponents/MainActivity.kt
+++ UIcomponents/app/src/main/java/com/example/uicomponents/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.uicomponents.ui.theme.UIcomponentsTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -20,58 +20,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UIcomponentsTheme {
-                val sheetState = rememberModalBottomSheetState()
-                val scope = rememberCoroutineScope()
-                var showBottomSheet by remember { mutableStateOf(false) }
-                Scaffold(
-                    floatingActionButton = {
-                        ExtendedFloatingActionButton(
-                            text = { Text("Show bottom sheet") },
-                            icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                            onClick = {
-                                showBottomSheet = true
-                            }
-                        )
-                    }
-                ) { paddingValues ->
-                    Column(
-                        modifier = Modifier
-                            .padding(paddingValues)
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        if (showBottomSheet) {
-                            ModalBottomSheet(
-                                onDismissRequest = {
-                                    showBottomSheet = false
-                                },
-                                sheetState = sheetState
-                            ) {
-                                Column(
-                                    modifier = Modifier
-                                        .padding(16.dp)
-                                        .fillMaxWidth()
-                                        .wrapContentHeight(),
-                                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ) {
-                                    Text("Bottom Sheet Content")
-                                    Button(onClick = {
-                                        scope.launch { sheetState.hide() }.invokeOnCompletion {
-                                            if (!sheetState.isVisible) {
-                                                showBottomSheet = false
-                                            }
-                                        }
-                                    }) {
-                                        Text("Hide bottom sheet")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                AlertDialogExample(
+                    onDismissRequest = {},
+                    onConfirmation = {},
+                    dialogTitle = "Alert Dialog",
+                    dialogText = "This is an alert dialog example using Jetpack Compose.",
+                    icon = Icons.Default.Info
+                )
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AlertDialogExample(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Alert Icon")
+        },
+        title = {
+            Text(text = dialogTitle, fontSize = 26.sp)
+        },
+        text = {
+            Text(text = dialogText, style = MaterialTheme.typography.bodyLarge)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                },
+                modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+            ) {
+                Text("OK", style = MaterialTheme.typography.bodyMedium)
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                },
+                modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+            ) {
+                Text("Cancel", style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+    )
 }
