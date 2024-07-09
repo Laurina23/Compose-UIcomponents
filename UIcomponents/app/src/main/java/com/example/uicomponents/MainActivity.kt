06ba41ba9587dde5cb4ebe5f6562f727ba4d23f9
Laurina-p <laurinapatnaik20@gmail.com>
2024-07-09 22:58:51 +0530

    Implemented State

--- UIcomponents/app/src/main/java/com/example/uicomponents/MainActivity.kt
+++ UIcomponents/app/src/main/java/com/example/uicomponents/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.uicomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.uicomponents.ui.theme.UIcomponentsTheme
@@ -19,16 +26,72 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UIcomponentsTheme {
-                var text by remember { mutableStateOf(TextFieldValue()) }
+                CounterButton()
+            }
+        }
+    }
+}
+@Composable
+fun CounterButton() {
+    var count by remember {
+        mutableStateOf(0)
+    }
 
-                OutlinedTextField(
-                    value = text,
-                    onValueChange = { text = it },
-                    label = { Text("Enter a text", fontSize = 14.sp) },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp)
-                )
+    val isPositive = count > 0
+    val isNegative = count < 0
+
+    UIcomponentsTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(36.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Count Value is : $count",
+                fontSize = 18.sp,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Current State:",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 8.dp),
+                    )
+                    if (count == 0) {
+                        Text(text = "Zero")
+                    } else if (isPositive) {
+                        Text(text = "Positive")
+                    } else {
+                        Text(text = "Negative")
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { count-- }) {
+                    Text(text = "Decrement")
+                }
+
+                Button(onClick = { count++ }) {
+                    Text(text = "Increment")
+                }
             }
         }
     }
